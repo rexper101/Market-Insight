a09d479189cef953e044edf76e9c71e44caac01a
Aniket Gauliwar <aniketgauliwar395@gmail.com>
2026-08-28 22:43:24 +0530

    changes in logger

--- MarketInsight/utils/logger.py
+++ MarketInsight/utils/logger.py
@@ -34,6 +34,11 @@ def get_logger(name: str = __name__) -> logging.Logger:
         console_handler.setLevel(logging.WARNING)
         console_handler.setFormatter(formatter)
 
+        root_logger = logging.getLogger()
+        root_logger.setLevel(logging.DEBUG)
+        root_logger.addHandler(file_handler)
+        root_logger.addHandler(console_handler)
+
         _LOGGING_CONFIGURED = True
 
     return logger
