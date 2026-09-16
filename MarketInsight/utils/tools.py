60559ba419949b6815b86fa523b0c68144c2ef46
Aniket Gauliwar <aniketgauliwar395@gmail.com>
2026-09-16 18:20:26 +0530

    changes in tools

--- MarketInsight/utils/tools.py
+++ MarketInsight/utils/tools.py
@@ -428,6 +428,3 @@ def get_ticker(company_name: str):
         else:
             return "Error: Failed to retrieve ticker. Please try again later."
             
-    except Exception as e:
-        logger.error(f"Failed to retrieve ticker of {company_name}: {str(e)}")
-        return "Error: Failed to retrieve ticker. Please try again later."
