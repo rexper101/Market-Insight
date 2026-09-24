f5fa6d393f4336ee4c0dfbd9ea2a15a7de5f37a2
Aniket Gauliwar <aniketgauliwar395@gmail.com>
2026-09-24 18:35:13 +0530

    changes in agent

--- MarketInsight/components/agent.py
+++ MarketInsight/components/agent.py
@@ -14,12 +14,3 @@ model = ChatOpenAI(
         base_url = "https://api.thesys.dev/v1/embed/"
     )
 
-agent = create_agent(
-    model,
-    tools = [get_stock_price, get_historical_data, get_stock_news, get_balance_sheet, get_income_statement, get_cash_flow,
-            get_company_info, get_dividends, get_splits, get_institutional_holders, get_major_shareholders,
-            get_mutual_fund_holders, get_insider_transactions, get_analyst_recommendations, get_analyst_recommendations_summary, get_ticker],
-    checkpointer = MemorySaver()
-)
-
-logger.info("Agent Initiated Successfully")
