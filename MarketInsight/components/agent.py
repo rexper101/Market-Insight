c71cbf6f1c5495fdd0c3269a21d36660d6e9df44
Aniket Gauliwar <aniketgauliwar395@gmail.com>
2026-09-24 18:35:42 +0530

    improvement in code

--- MarketInsight/components/agent.py
+++ MarketInsight/components/agent.py
@@ -6,11 +6,3 @@ from MarketInsight.utils.logger import get_logger
 from langgraph.checkpoint.memory import MemorySaver
 from langchain.agents import create_agent
 
-load_dotenv()
-logger = get_logger(__name__)
-
-model = ChatOpenAI(
-        model = "c1/openai/gpt-5/v-20250930",
-        base_url = "https://api.thesys.dev/v1/embed/"
-    )
-
